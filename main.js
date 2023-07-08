15e81395ff0a3a83ac594e1d10d5598b8616a9f2
karmut <karina.mutafyan@gmail.com>
2023-07-08 16:17:05 +0200

    change 1

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-//1
-// let data = [1, 3, 67, 4, 7, 8, 23, 99, 67, 54, 3, 0, 8, 7, 43];
+1;
+let data1 = [1, 3, 67, 4, 7, 8, 23, 99, 67, 54, 3, 0, 8, 7, 43];
 
-// function modify(element) {
-//   let r = element * 100;
-//   return r;
-// }
+function modify(element) {
+  let r = element * 100;
+  return r;
+}
 
-// let newArray = data.map(modify);
+let new1Array = data.map(modify);
 
-// console.log(newArray);
+console.log(new1Array);
 //------------------------------------
 //2
 
@@ -20,15 +20,15 @@ function changeToValue(e) {
 //------------------------
 //3
 
-// let numbers = [
-//   3, 7, 5, 4, 67, 8, 844, 74, 34, 6, 7888, 5, 6765, 4, 56, 67, 4, 6,
-// ];
-// let newArray = numbers.filter(function (e) {
-//   if (e < 100) {
-//     return false;
-//   } else {
-//     return true;
-//   }
-// });
+let numbers = [
+  3, 7, 5, 4, 67, 8, 844, 74, 34, 6, 7888, 5, 6765, 4, 56, 67, 4, 6,
+];
+let newArray = numbers.filter(function (e) {
+  if (e < 100) {
+    return false;
+  } else {
+    return true;
+  }
+});
 
-// console.log(newArray);
+console.log(newArray);
