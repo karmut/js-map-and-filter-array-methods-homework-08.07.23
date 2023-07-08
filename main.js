9a35a57b1dd7647977d157ca73f876180dd39e82
karmut <karina.mutafyan@gmail.com>
2023-07-08 16:15:10 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+//1
+// let data = [1, 3, 67, 4, 7, 8, 23, 99, 67, 54, 3, 0, 8, 7, 43];
+
+// function modify(element) {
+//   let r = element * 100;
+//   return r;
+// }
+
+// let newArray = data.map(modify);
+
+// console.log(newArray);
+//------------------------------------
+//2
+
+let data = ["karina", "Kaktus", "Harry Potter"];
+function changeToValue(e) {
+  data.lenght;
+}
+
+//------------------------
+//3
+
+// let numbers = [
+//   3, 7, 5, 4, 67, 8, 844, 74, 34, 6, 7888, 5, 6765, 4, 56, 67, 4, 6,
+// ];
+// let newArray = numbers.filter(function (e) {
+//   if (e < 100) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// });
+
+// console.log(newArray);
